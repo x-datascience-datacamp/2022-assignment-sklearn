f61b430466e2fe35581cd9e359416400100c4f8d
Alexis Bouley <alexisbouley@MacBook-Air-de-Alexis.local>
2023-01-10 15:32:40 +0100

    UP my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -123,6 +123,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
 
     def score(self, X, y):
         """Calculate the score of the prediction.
+
         Parameters
         ----------
         X : ndarray, shape (n_samples, n_features)
