ead1948f83b83485456f715f5e99ead881f372a2
XL85 <86225232+XL85@users.noreply.github.com>
2023-01-10 18:53:21 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -72,6 +72,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
 
     def fit(self, X, y):
         """Fitting function.
+        
          Parameters
         ----------
         X : ndarray, shape (n_samples, n_features)
@@ -96,6 +97,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
 
     def predict(self, X):
         """Predict function.
+        
         Parameters
         ----------
         X : ndarray, shape (n_test_samples, n_features)
@@ -121,6 +123,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
 
     def score(self, X, y):
         """Calculate the score of the prediction.
+        
         Parameters
         ----------
         X : ndarray, shape (n_samples, n_features)
