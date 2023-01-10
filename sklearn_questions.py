05bb02378bec80b181f73e04ffe074ee5a7efcfe
xAhmad1 <ahmad1181@live.com>
2023-01-11 07:27:53 +0800

    docstring

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -176,7 +176,6 @@ class MonthlySplit(BaseCrossValidator):
         n_splits : int
             The number of splits.
         """
-
         X = X.reset_index()
         X = X.set_index(self.time_col)
         if (X.index.inferred_type != "datetime64"):
@@ -211,7 +210,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         n_splits = self.get_n_splits(X, y, groups)
         X = X.reset_index()
         X = X.set_index(self.time_col)
