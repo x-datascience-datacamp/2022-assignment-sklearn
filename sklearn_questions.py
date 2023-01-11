0689d5d211f59a4bdef6e04810d921659b2dcea7
Eiznar <user1@DESKTOP-H6CB8K1.localdomain>
2023-01-12 00:55:39 +0100

    sklearn Eiznar solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -105,6 +105,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         self.X_ = X
         self.y_ = y
         self.classes_ = np.unique(y)
+        self.n_features_in_ = self.X_.shape[1]
 
         return self
 
