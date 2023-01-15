48b5f9b343454ac190a8278bfbd93266d64a148d
Adem Jaziri <ademjaziri410@gmail.com>
2023-01-15 18:09:06 +0100

    sklearn solutions

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -139,8 +139,8 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
 
 
 class MonthlySplit(BaseCrossValidator):
-
     """CrossValidator based on monthly split.
+
     Split data based on the given `time_col` (or default to index). Each split
     corresponds to one month of data for the training and the next month of
     data for the test.
@@ -158,7 +158,6 @@ class MonthlySplit(BaseCrossValidator):
         self.time_col = time_col
 
     def get_n_splits(self, X, y=None, groups=None):
-
         """Return the number of splitting iterations in the cross-validator.
 
         Parameters
@@ -176,7 +175,6 @@ class MonthlySplit(BaseCrossValidator):
         n_splits : int
             The number of splits.
         """
-
         if type(X.index) != pd.RangeIndex:
             X = X.reset_index()
 
@@ -186,7 +184,6 @@ class MonthlySplit(BaseCrossValidator):
         return len(X.resample('M', on=self.time_col)) - 1
 
     def split(self, X, y, groups=None):
-
         """Generate indices to split data into training and test set.
 
         Parameters
@@ -206,7 +203,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         n_splits = self.get_n_splits(X, y, groups)
 
         def idx_array(array):
