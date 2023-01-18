0ac4948ceaa00513378ae36f2aa1126627a562d3
Yang ZHANG <92279546+yangzhang33@users.noreply.github.com>
2023-01-18 01:52:14 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -191,7 +191,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         X = X.reset_index()
         n_splits = self.get_n_splits(X, y, groups)
         date = X[self.time_col]
