4a6f5bbb13d6fc395df65604f263a1a417075ac3
Yang ZHANG <92279546+yangzhang33@users.noreply.github.com>
2023-01-18 01:50:02 +0100

    Update sklearn_questions.py

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -85,7 +85,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         self.X_, self.y_ = check_X_y(X, y)
         check_classification_targets(self.y_)
         self.classes_ = np.unique(self.y_)
-        self.n_features_in_ = X.shape[1]
+        self.n_features_in_ = self.X_.shape[1]
         return self
 
     def predict(self, X):
